509044b860b611e8c2f798c2a87ba2f1eecbc4e7
Arthur Bastide <bastide.arthur@gmail.com>
2021-06-02 17:27:16 +0200

    app.py V3

--- FOREST_GUARD_FRONT_END/app.py
+++ FOREST_GUARD_FRONT_END/app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 
+# Forest selection
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 
 load_dotenv(dotenv_path)
@@ -15,19 +16,19 @@ service_account = 'earth-engine@wagon-bootcamp-data.iam.gserviceaccount.com'
 credentials = ee.ServiceAccountCredentials(service_account, key_data=key_data)
 ee.Initialize(credentials)
 
-st.title('Forest guards')
+st.sidebar.title('FOREST GUARDS 🌳')
 
 # Forest selection
 df_zone = pd.DataFrame({'first column': ['Select a forest', 'Vosges', 'Black Forest']})
-option_zone = st.selectbox('Select a forest', df_zone['first column'])
+option_zone = st.sidebar.selectbox('Select a forest', df_zone['first column'])
 zones = {
         'Black Forest' : [49, 7.5],
-        'Vosges' : [48.5, 8.3]
+        'Vosges' : [48.454874373107636, 6.882905907505892]
         }
 
-
 # Year selection
-option_year = st.slider('Select a year', 2017, 2020)
+option_year_1 = st.sidebar.slider('Select a year', 2018, 2020)
+
 
 OPTICAL_BANDS = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
 THERMAL_BANDS = ['B10', 'B11']
@@ -38,6 +39,11 @@ FEATURES = BANDS + [RESPONSE]
 MODEL_NAME = 'JP_test_model_adam_binarycrossentropy'
 PROJECT = 'wagon-bootcamp-data'
 REGION = 'europe-west1'
+
+MODEL_NAME_SMALL = 'JP_test_model_adam_binarycrossentropy'
+VERSION_SMALL = "v1622563893"
+MODEL_NAME_LARGE = 'adam_binary_tf_none_none'
+VERSION_LARGE = 'v1622619546'
 
 def add_ee_layer(self, ee_image_object, vis_params, name):
   map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
@@ -70,9 +76,9 @@ def maskL8sr(image):
           image.select(THERMAL_BANDS).divide(10).clamp(273.15, 373.15)
             .subtract(273.15).divide(100)).updateMask(mask)
 
+
 # The image input data is a cloud-masked median composite.
-image = l8sr.filterDate(
-    '2018-01-01', '2020-12-31').map(maskL8sr).median().select(BANDS).float()
+
 image20 = l8sr.filterDate(
     '2020-01-01', '2020-12-31').map(maskL8sr).median().select(BANDS).float()
 image19 = l8sr.filterDate(
@@ -109,47 +115,94 @@ palette = [ '#FEFF99',
             # '0000FF'
           ]
 
-if option_zone != 'Select a forest':  
+
+
+
+# Display map
+if option_zone != 'Select a forest': 
+
   # Use folium to visualize the input imagery and the predictions.
-  mapid = years[option_year].getMapId({'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 0.3})
-  map = folium.Map(location=zones[option_zone], zoom_start=15)
+  mapid_1 = years[option_year_1].getMapId({'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 0.3})
+  map = folium.Map(location=zones[option_zone], zoom_start=12)
   folium.TileLayer(
-      tiles=mapid['tile_fetcher'].url_format,
+      tiles=mapid_1['tile_fetcher'].url_format,
       attr='Google Earth Engine',
       overlay=True,
-      name='landsat',
-      show=False
+      name=f'landsat_{option_year_1}',
+      show=True
     ).add_to(map)
 
-  ### Prediction
-  predictions = model.predictImage(image.toArray())
+  ### Prediction year 1
+  threshold = 0.80
+  predictions_year_1 = model.predictImage(years[option_year_1].toArray())
+  predictions_year_1=predictions_year_1.where(predictions_year_1.gt(threshold), 1)
+  predictions_year_1=predictions_year_1.where(predictions_year_1.lt(threshold), 0)
 
-  mask_pred = predictions.updateMask(predictions.gte(0.8))
+  mask_pred_year_1 = predictions_year_1.updateMask(predictions_year_1.gte(threshold))
 
-  map.add_ee_layer(mask_pred,
+  map.add_ee_layer(mask_pred_year_1,
                   {'bands': ['fnf'],
                         'min': 1, 
                         'max': 1, 
                         'palette':palette
-                        }, 'predictions_masked'
+                        }, f'predictions_{option_year_1}'
                   )
+
   ###JAXA
   jaxa = ee.ImageCollection('JAXA/ALOS/PALSAR/YEARLY/FNF').filterDate('2017-01-01', '2017-12-31').median()
 
+  palette_jaxa = ['006400','FEFF99','0000FF']
   mask_pred_jaxa = jaxa.updateMask(jaxa.lte(1.5))
 
   map.add_ee_layer(mask_pred_jaxa,
                   {'bands': ['fnf'],
                         'min': 1, 
                         'max': 3, 
-                        'palette':palette
+                        'palette':palette_jaxa
                         }, 'jaxa_masked'
                   )
 
+  if st.sidebar.checkbox('See forest evolution'):
+    option_year_2 = st.sidebar.slider('Select another year to see forest evolution', 2018, 2020)
+
+    #if option_year_2 > option_year_1
+
+    ### Prediction year 2
+    predictions_year_2 = model.predictImage(years[option_year_2].toArray())
+    predictions_year_2=predictions_year_2.where(predictions_year_2.gt(threshold), 1)
+    predictions_year_2=predictions_year_2.where(predictions_year_2.lt(threshold), 0)
+
+    mask_pred_year_2 = predictions_year_2.updateMask(predictions_year_2.gte(threshold))
+
+    map.add_ee_layer(mask_pred_year_2,
+                    {'bands': ['fnf'],
+                          'min': 1, 
+                          'max': 1, 
+                          'palette':palette
+                          }, f'predictions_{option_year_2}'
+                    )      
+
+    #Diff of prediction
+    palette_diff = [ 'red','blue']
+
+    
+    diff = predictions_year_2.subtract(predictions_year_1)
+    d_map = predictions_year_1.multiply(0)
+    d_map = d_map.where(diff.gt(0.6), 2)      # All pos diffs are now labeled 2.
+    d_map = d_map.where(diff.lt(-0.6), 1)      # Label all neg to 1.
+    mask_d_map = d_map.updateMask(d_map.gte(0.5)) #mask the zeros
+
+
+    map.add_ee_layer(mask_d_map,
+                    {'bands': ['fnf'],
+                          'min': 1, 
+                          'max': 2, 
+                          'palette':palette_diff
+                          }, f'predictions_diff_{option_year_2}_{option_year_1}'
+                    )
 
   map.add_child(folium.LayerControl())
 
   folium_static(map)
-
 else:
   pass
